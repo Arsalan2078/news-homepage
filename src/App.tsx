9a6f003a4137a7f241322460100fb7f127ecaac8
Arsalan Rabdanov <arsalan.rabdanov2078@gmail.com>
2025-07-10 07:23:11 -0400

    app grid layout done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import "./App.module.scss";
+import "./App.scss";
 
 function App() {
-  return <>App</>;
+  return (
+    <>
+      <div className="app">
+        <header className="header">
+          <div className="container">Header</div>
+        </header>
+
+        <main className="main">
+          <div className="container main__grid">
+            <section>Section 1: The Bright Future of Web 3.0</section>
+            <section>Section 2: New Articles</section>
+            <section>Section 3: Top Articles</section>
+          </div>
+        </main>
+      </div>
+    </>
+  );
 }
 
 export default App;
